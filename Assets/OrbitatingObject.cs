3b636db77354f7fc7c0ddc06fa514a1b3e2800a6
Luis Armas <luarsu@gmail.com>
2019-01-26 11:42:00 +0000

    push for the orbit script

--- Assets/OrbitatingObject.cs
+++ Assets/OrbitatingObject.cs
@@ -6,14 +6,18 @@ public class OrbitatingObject : MonoBehaviour {
 
     public GameObject OrbitCenter;
 
-	// Use this for initialization
-	void Start () {
-        //OrbitCenter = Get
+    [Tooltip("Orbitatng speed, in units/second")]
+    [Range(0, 100)]
+    public float Speed = 50;
 
+    // Use this for initialization
+    void Start () {
+        OrbitCenter = GameObject.FindWithTag("Earth");
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        //Rotate function
+        transform.RotateAround(OrbitCenter.transform.position, Vector3.up, Speed * Time.deltaTime);
+    }
 }
