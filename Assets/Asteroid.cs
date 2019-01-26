180646eda1c3cf66e3fc82439f2bc737ce896c65
Luis Armas <luarsu@gmail.com>
2019-01-26 16:46:49 +0000

    implemented the asteroid push mechanic

--- Assets/Asteroid.cs
+++ Assets/Asteroid.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Asteroid : MonoBehaviour {
+public class Asteroid : MonoBehaviour
+{
 
     public GameObject OrbitCenter;
 
@@ -10,6 +11,11 @@ public class Asteroid : MonoBehaviour {
     [Range(0, 100)]
     public float Speed = 50;
 
+    public float HitStrength;
+
+    private bool applyForce = false;
+
+    private Rigidbody hitObject;
     // Use this for initialization
     void Start()
     {
@@ -22,4 +28,27 @@ public class Asteroid : MonoBehaviour {
         //Rotate function
         transform.RotateAround(OrbitCenter.transform.position, Vector3.up, Speed * Time.deltaTime);
     }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        hitObject = collision.gameObject.GetComponent<Rigidbody>();
+
+        //if the hit object has a rigidbody
+        if (hitObject != null)
+        {
+            applyForce = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (applyForce)
+        {
+            Debug.Log("Force Applied");
+            Vector3 directionHit = hitObject.gameObject.transform.position - transform.position;
+            hitObject.AddForce(directionHit * HitStrength);
+            applyForce = false;
+        }
+    }
+
 }
