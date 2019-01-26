283abebe8959d372e3d3005145dc5709ce3bf458
Luis Armas <luarsu@gmail.com>
2019-01-26 12:41:19 +0000

    Added gravity feature for humans

--- Assets/PoorHuman.cs
+++ Assets/PoorHuman.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class PoorHuman : MonoBehaviour {
 
+    public Rigidbody HumanRigidbody;
+
+    public GameObject atractingPlanet;
+
+    public float gravity;
 	// Use this for initialization
 	void Start () {
-		
-	}
+		HumanRigidbody = GetComponent<Rigidbody>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+        if (atractingPlanet != null)
+        {
+            //check that they are still within the radious
+            //Not implemented yet
+
+            //Calculate direction to planet
+            Vector3 directionToplanet = atractingPlanet.transform.position - transform.position;
+
+            //Apply the force
+            HumanRigidbody.AddForce(directionToplanet * gravity);
+        }
 	}
+
+    //Check trigger enter of the planet gravity
+    private void OnTriggerEnter(Collider other)
+    {
+        //if its a planet assign the radious
+        if (other.tag == "Planet" || other.tag == "Earth")
+        {
+            atractingPlanet = other.gameObject;
+        }
+    }
 }
