33745e1bfad63a0164c897789ba9fe4e7c8ca128
Samuel Sarfaty <samuel.sarfaty@gmail.com>
2019-01-26 11:59:42 +0000

    Added planet with slider functionality

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Planet : MonoBehaviour {
+
+	[SerializeField] int maxPeople = 0;
+	[SerializeField] int currentPeople = 0;
+
+	[SerializeField] Slider mySlider;
+
+	void Awake(){
+		mySlider.maxValue = maxPeople;
+	}
+
+	void UpdatePeopleCounter(){
+		mySlider.value = currentPeople;
+	}
+
+
+}
