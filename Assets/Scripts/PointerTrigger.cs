c4aff3b81962623aeb54d19e3c9b2faf711f8339
Samuel Sarfaty <samuel.sarfaty@gmail.com>
2019-01-27 11:37:47 +0000

    level update

---
+++ Assets/Scripts/PointerTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerTrigger : MonoBehaviour {
+
+	private MeshRenderer earthRenderer;
+
+	void Awake(){
+		earthRenderer = GameObject.FindWithTag ("Earth").GetComponentInChildren<MeshRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(earthRenderer.isVisible){
+			//print ("I see earth");
+		} else {
+			//print ("no earth");
+		}
+	}
+}
